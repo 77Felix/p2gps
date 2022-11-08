7a4ae571a8859708325042ff770d91467734b53a
FÉLIX CUEVAS MEDINA <Felix.Cuevas@alu.uclm.es>
2022-11-08 12:57:42 +0100

    Igualar local y remoto

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#no lanzar el .jar hasta que no esté accesible al servicio de MongoDB
+#el host 'mongo' tendrá una IP otorgada al lanzar la pila de #servicios,
+#y le damos el apodo 'mongo', utilizando aquí y en el #archivo application.properties
 
 while ! curl http://mongo:27017/
 do
@@ -7,4 +10,4 @@ do
 done
 echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config.location=application.properties
